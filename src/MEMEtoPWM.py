ed769a2444c8495f860f67d2f5ecb982e30cac99
Tuomo <tuomo.hartonen@helsinki.fi>
2021-06-18 13:18:54 +0200

    Fixed a bug that caused the last motif from MEME output to be missed. Added option to filter motifs based on MEME p-value.

--- src/MEMEtoPWM.py
+++ src/MEMEtoPWM.py
@@ -15,7 +15,8 @@ def MEMEtoPWM():
     parser.add_argument("MEME", help="Full path to the input MEME-file.", type=str)
     #outdir = directory for output
     parser.add_argument("outdir", help="Full path to output directory where PWMs are saved. Motif names from the MEME-file are used as file names.", type=str)
-
+    #optional paramenters
+    parser.add_argument("-p",help="Optional p-value threshold for filtering out motifs with larger p-value than -p (default=1.0).",type=float,default=1.0)
     args = parser.parse_args()
     
     motif = None
@@ -29,12 +30,13 @@ def MEMEtoPWM():
                 motif_found = True
                 if motif is not None:
                     #saving the last motif to a file
-                    print("saving "+motifname) 
-                    np.savetxt(args.outdir+motifname+".pwm",np.transpose(np.array(motif)))
+                    if pval<args.p:
+                        print("saving "+motifname) 
+                        np.savetxt(args.outdir+motifname+".pwm",np.transpose(np.array(motif)))
                 motifname = line.replace(" ","-").strip()
                 motif = []
             elif not motif_found: continue    
-            elif line.count('letter')>0: continue
+            elif line.count('letter')>0: pval = float(line.split()[-1])
             elif line[0]=='*':
                 #this means that all motifs have been read in
                 break
@@ -43,6 +45,9 @@ def MEMEtoPWM():
                 line = line.split()
                 motif.append([float(i) for i in line])
 
-
+    #saving the last motif
+    if pval<args.p:
+        print("saving "+motifname)
+        np.savetxt(args.outdir+motifname+".pwm",np.transpose(np.array(motif)))
 #end
 MEMEtoPWM()
